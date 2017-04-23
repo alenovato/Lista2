94fc08f7e79b640d052f354070cb5da451f4c820
Vine19 <viniciusdc1915@gmail.com>
2017-04-23 17:24:19 -0400

    Lista de exercicios

---
+++ exercicio13/TimeDeFutebol.java
@@ -0,0 +1,13 @@
+package Exercicio13;
+
+
+public class TimeDeFutebol {
+
+	public static void main(String[] args) {
+
+		ObjetoTimeDeFutebol time1 = new ObjetoTimeDeFutebol("São Paulo FC", "Junior", "Rogerio Ceni", "3 Mundiais");
+
+		System.out.println(time1);
+
+	}
+}
